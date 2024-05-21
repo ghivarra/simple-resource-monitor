582bd1e77999cfe4311eb13ab83deb58f9e774d6
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-05-21 15:48:42 +0700

    create options for the bash script

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-############################################################
-# Variables                                                #
-############################################################
-UserName=webmaster
-DomainName=monitor.test
+# Usage Script
+usage() { 
+    # Display Help
+    echo "You need to use the options below to run the script"
+    echo
+    echo "Syntax: scriptTemplate [-d|h]"
+    echo "options:"
+    echo "-h     Print this Help."
+    echo "-d     Your root directory for this script."
+    echo
+}
+
+# Get options
+while getopts ":hd:" option; do
+    case $option in
+        d) # Enter Directory
+            Directory=$OPTARG;;
+        h) # Help
+            usage
+            exit;;
+        \?) # Invalid option
+            usage
+            exit;;
+    esac
+done
 
 ############################################################
 # Scripts                                                  #
 ############################################################
-echo $(free -m) > /home/$UserName/www/$DomainName/memory.txt
-echo $(mpstat -P ALL -o JSON) > /home/$UserName/www/$DomainName/cpu.json
+echo $(free -m) > $Directory/memory.txt
+echo $(mpstat -P ALL -o JSON) > $Directory/cpu.json
